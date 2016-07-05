798ce65f6ee6ba9361a64e21e5f4fab824958867
heyimhuman <kurusagi@kurusagideMacBook-Pro.local>
2016-07-05 13:39:47 +0800

    加了列表

---
+++ First/MainTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MainTableViewCell.h
+//  First
+//
+//  Created by kurusagi on 16/7/5.
+//  Copyright © 2016年 kurusagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *MainTextLabel;
+
+@end
---
+++ First/MainTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainTableViewController.h
+//  First
+//
+//  Created by kurusagi on 16/7/5.
+//  Copyright © 2016年 kurusagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+
+@end
